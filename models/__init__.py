82a08900a824698f904158460df1008516a7f6fc
Fayeben <18317187920@163.com>
2024-05-30 14:59:55 +0800

    revision

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 from .build import build_model_from_cfg
 import models.dvae
 import models.Point_BERT
-import models.Point_BERT_shallow
-import models.Point_BERT_deep
+import models.PCPT_shallow
+import models.PCPT_deep
 import models.Point_OAE_pcn
 import models.util_models
